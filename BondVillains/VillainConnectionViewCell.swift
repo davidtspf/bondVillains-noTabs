e384cd4882aa5dfc9894c7513baff80582cb0acb
David Tannenbaum <davidtspf@users.noreply.github.com>
2020-06-14 14:33:02 -0700

    First commit

---
+++ BondVillains/VillainConnectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  VillainConnectionViewCell.swift
+//  BondVillains
+//
+//  Created by DT on 6/14/20.
+//  Copyright © 2020 Udacity. All rights reserved.
+//
+
+import UIKit
+
+class VillianCollectionViewCell: UICollectionViewCell {
+    
+    // Mark: Outlets
+
+    @IBOutlet weak var villainImageView: UIImageView!
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+}
---
+++ BondVillains/VillainConnectionViewController.swift
@@ -0,0 +1,46 @@
+//
+//  VillainConnectionViewController.swift
+//  BondVillains
+//
+//  Created by DT on 6/14/20.
+//  Copyright © 2020 Udacity. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class VillainConnectionViewController: UICollectionViewController {
+    
+    // Get ahold of some villains, for the table
+    // This is an array of Villain instances.
+    let allVillains = Villain.allVillains
+    
+    // MARK: Life Cycle
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        self.tabBarController?.tabBar.isHidden = false
+    }
+    
+    // MARK: Collection View Data Source
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.allVillains.count
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath:IndexPath) {
+
+        // Grab the DetailVC from Storyboard
+        let detailController = self.storyboard!.instantiateViewController(withIdentifier: "VillainDetailViewController") as! VillainDetailViewController
+
+        //Populate view controller with data from the selected item
+        detailController.villain = allVillains[(indexPath as NSIndexPath).row]
+
+        // Present the view controller using navigation
+        navigationController!.pushViewController(detailController, animated: true)
+
+    }
+    
+    
+    
+}
